4ca45cdb7446a3479ab2e93eda4d68d5607625eb
HamzaFarooq3333 <hamzafarooqsea@gmail.com>
2025-01-04 14:55:13 +0500

    How to take input from user

---
+++ Code.py
@@ -0,0 +1,13 @@
+a=input("Enter your name : ")
+print("The name is : ",a)
+
+
+a=input("enter first number = ")
+b=input("enter second number = ")
+#Number treated as string concatination 
+print("Number treated as string concatination ")
+print("sum of two number is = ",a+b)
+
+#Number treated as integers arithematic operation
+print("Number treated as integers arithematic operation")
+print("sum of two number is = ",int(a)+int(b))
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Python_Tutorial
-This is a repository containing inforamtion about basic python 
+We can take user input from the user in our code using input() function
+<br>
+This function gives the value as a string/character which we have to pass into a varaible
+<br>
+Input function returns value in a string thus we have to typecast it 
+<br>
+If we sum the input numbers then they will be added as string instead  of numbers
