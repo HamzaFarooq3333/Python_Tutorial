87c50a016946b52f72f1af6c43b6bdd6e2e16967
HamzaFarooq3333 <hamzafarooqsea@gmail.com>
2025-01-07 23:37:04 +0500

    Added info on functions

--- Code.py
+++ Code.py
@@ -1,246 +1,67 @@
-name="hamza"
-print(name)
+# def calculateMean(a,b):
+#   mean=(a*b)/(a+b)
+#   print("The mean is = ",mean)
 
 
+# def isGreater(a,b):
+#   if(a>b):
+#     print("First number is greater than the other ")
+#   else:
+#     print("Second number is greater than the other ")
 
 
-#Double quote inside string 
-#Using escape sequence characters
-a="He said ,\"I want to eat an apple"
-print(a)
+# def isLess(a,b):
+#   pass   #Ignore this and pass on the next code we will write this later it helps avoiding error 
+#          # in case we did not define the function 
 
-#Using double quote in a single quote
-a='He said ,"I wants to eat an apple"'
-print(a)
+# c=int(input("Enter first number = "))
+# d=int(input("Enter second number = "))
+# calculateMean(c,d)
+# isGreater(c,d)
+# isLess(c,d)
 
 
-#Printing multiple lines in a single string using triple single or double quotes
-b='''Hello
-My name is  hamza
-I am a CS  student'''
-print(b)
+#Methods to pass arguments to a function 
 
+#Required arguments
+def Average(a,b):
+  print("The average is = ",(a+b)/2)
+  
+Average(2,5)
 
-####Indexing in strings 
-print(b[3])
-print(b[7])
+#Default Arguments
+def defaultAverage(a=3,b=5):
+  print("The average is = ",(a+b)/2)
 
-#Displaying all characters in a string using for loop
-print("Displaying all characters in a string using for loop")
-for i in b:
-    print(i)
+defaultAverage()
 
+#Keyword Arguments
+def greeting(greeting ="Hello ",fname="Hamza",lname="Faroooq"):
+  print(greeting+" "+fname+" "+lname)
 
-#String slicing
-name="Hamza Farooq"
-print(name[0:5])
-print(name[6:18])
 
+greeting(lname="Ahmad",fname="Ali",greeting=
+         "Assalamu Alaikum")
 
-fruit='mango'
-print(len(fruit))
 
-#This includes 0 but not 3
-print(fruit[:3]) #Python automatically sets the initial index to zero 
 
-print(fruit[3:]) #Python automatically sets the end index to the last index of the string
-#Above print is also equal to print(fruit[3:len(fruit))
-print(fruit[:]) #Python automatically sets the first and last index of the string accordingly
+#Variable Arguments
 
+#Arbitary Argument 
+def arbitaryAverage(*numbers):
+ print(type(numbers))
+ sum =0
+ for i in numbers:
+   sum = sum + i
+   print("The average is = ",sum/len(numbers))
+  
 
-#String slicing using negative indexing 
-print(fruit[0:-3]) 
-#Equivalent to
-print(fruit[0:len(fruit)-3])
+arbitaryAverage(4,2,4,6,6,7)
 
+#Arbitary keyword argument
+def name(**names):
+  print(type(names))
+  print("Hello",names["fname"],names["lname"])
 
-#print(fruit[-1,-3])
-#Equal to print(fruit[len(fruit)-1,len(fruit)-3])
-#Which is 4:2 therefore nothing is printed begin should be smaller then end
 
-#print(fruit[-3,-1])
-
-
-#####String Methods (Functions built-in in strings)
-fruit="!!ManGo!!"
-
-
-#1. lower() - converts all characters in a string to lowercase
-print("--> lower() - converts all characters in a string to lowercase")
-print(fruit.lower())
-
-
-#2. upper() - converts all characters in a string to uppercase
-print("--> upper() - converts all characters in a string to uppercase")
-print(fruit.upper())
-
-
-#3. title() - converts the first character of each word to uppercase and the rest to lowercas
-print("--> title() - converts the first character of each word to uppercase and the rest to lowercase")
-print(fruit.title())
-
-
-
-#4. capitalize() - converts the first character of a string to uppercase and the rest to lowercas
-print("--> capitalize() - converts the first character of a string to uppercase and the rest to lowercase")
-print(fruit.capitalize())
-
-
-#4. swapcase() - swaps the case of a string (i.e. upper case becomes lower and lower becomes upper
-print("--> swapcase() - swaps the case of a string (i.e. upper case becomes lower and lower becomesupper")
-print(fruit.swapcase())
-
-
-#5. count() - returns the number of occurrences of a substring in a string
-print("--> count() - returns the number of occurrences of a substring in a string")
-print(fruit.count('a'))
-
-
-#6. find() - returns the index of the first occurrence of a substring in a string
-print("--> find() - returns the index of the first occurrence of a substring in a string -1 if not present")
-print(fruit.find('i'))
-
-
-#7. rfind() - returns the index of the last occurrence of a substring in a string
-print("--> rfind() - returns the index of the last occurrence of a substring in a string -1 if not found")
-print(fruit.rfind('i'))
-
-
-#8. index() - returns the index of the first occurrence of a substring in a string
-print('''-->  index() - returns the index of the first occurrence of a substring in a string 
-     throws error if substring is not present in the string  ''')
-print(fruit.index('o'))
-
-
-fruit.isalnum
-#9. isalnum() - returns True if all characters in a string are alphanumeric, otherwise Fals
-print("--> isalnum() - returns True if all characters in a string are alphanumeric(A-Z or a-z or 0-9), otherwise False")
-print(fruit.isalnum())
-
-
-fruit.isalpha
-#10. isalpha() - returns True if all characters in a string are alphabets,
-print("--> isalpha() - returns True if all characters in a string are alphabets(A-Z or a-z), otherwise retruns false")
-print(fruit.isalpha())
-
-
-#11. isdigit() - returns True if all characters in a string are digits, otherwise Fals
-print("--> isdigit() - returns True if all characters in a string are digits(0-9)")
-print(fruit.isdigit())
-
-
-
-#12. islower() - returns True if all characters in a string are lower case, otherwis
-print("--> islower() - returns True if all characters in a string are lower case")
-print(fruit.islower())
-
-
-#13. isupper() - returns True if all characters in a string are upper case, otherwis
-print("-->  isupper() - returns True if all characters in a string are upper case")
-print(fruit.isupper())
-
-
-
-#14. isspace() - returns True if all characters in a string are spaces, otherwise F
-print("--> isspace() - returns True if all characters in a string are spaces")
-print(fruit.isspace())
-
-
-
-#15. istitle() - returns True if a string is a title, i.e. th
-print("""--> istitle() - returns True if a string is a title, i.e. the first
-      character of each word is upper case and the rest are lower case""")
-print(fruit.istitle())
-
-
-
-#16. isprintable() - returns True if all characters in a string are printable, otherwis
-print("-->  isprintable() - returns True if all characters in a string are printable (/n) is not a printable character")
-print(fruit.isprintable())
-
-
-
-#17. isnumeric() - returns True if all characters in a string are numeric, otherwise F
-print("-->  isnumeric() - returns True if all characters in a string are numeric")
-print(fruit.isnumeric())
-
-
-
-#18. startswith() - returns True if a string starts with a specified value, otherwise Fals
-print("-->  startswith() - returns True if a string starts with a specified value, otherwise False")
-print(fruit.startswith('M'))
-
-
-
-#19. endswith() - returns True if a string ends with a specified value, otherwise Fals
-print("-->  endswith() - returns True if a string ends with a specified value, otherwise False")
-print(fruit.endswith('o'))
-print("""Can also be used by slicing  end_with("word",starting_index,ending_index)""")
-print(fruit.endswith("o",2,4))
-
-
-
-#20. replace() - returns a string where all occurrences of a substring are replaced with another substring
-print("--> replace() - returns a string where all occurrences of a substring are replaced with another substring")
-print(fruit.replace('a','b'))
-
-
-
-#21. split() - splits a string into a list where each word is a list item
-print("-->  split() - splits a string into a list where each word is a list item")
-print(fruit.split('o'))
-
-
-
-#22. join() - joins all items in a list into a string
-print("-->  join() - joins all items in a list into a string")
-print(' '.join(['apple','banana','cherry']))
-
-
-
-#23. strip() - removes leading and trailing characters from a string
-print("-->  strip() - removes leading and trailing characters from a string")
-print(fruit.strip('!'))
-
-
-
-#24. lstrip() - removes leading characters from a string
-print("--> lstrip() - removes leading characters from a string")
-print(fruit.lstrip('!'))
-
-
-
-#25. rstrip() - removes trailing characters from a string
-print("--> rstrip() - removes trailing characters from a string")
-print(fruit.rstrip('!'))
-
-
-
-#26. center() - returns a string centered in a specified width, padding with a specified fill
-print("--> () - returns a string centered in a specified width, padding with a specified fill")
-print(fruit.center(20))
-print(fruit.center(20,"-"))
-print(len(fruit.center(20)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+name(fname="Hamza",lname="farooq")
--- README.md
+++ README.md
@@ -1,25 +1,48 @@
-Strings are immuteable(cannot be changed) when you use function like upper you get a new string 
+Function is a several lines of code that we can define for a specific purpose
 <br>
-The existing string does not change but intead you get a new string from that string 
+We can use that code many times without writing the same code we just call that function
 <br>
-Anything in single or double quotes in python is a string it is like an array of characters
+It is very useful because if there is a mistake in the code we do not need to fix that mistake at every place we used that code
+<br> 
+We just need to fix the code in the function once 
 <br>
-Multiplw lines in a string can be written using triple double or single quote
+It helps reuse the code without copy pasting it again and again when needed
 <br>
-Indexing allows us to display a specific character from our string but we cannot get out of the index bound 
+Funtion are of 2 types
 <br>
-We can slice a string displaying a specific part of the string by using starting and ending index
+1. Built in function(Already present in python for use in code)
 <br>
-We can use the len() function to get the length of the string
+2. User defined function(We create our own function for use in code)
 <br>
+->We use pass in function it tells the compiler that we will define this function later so pass on this code after this function 
+<br>
+->We can use return in function to return a value from the function
+<br>
+<br>
+There are 4 types of arguments that we can provide in a function 
+<br>
+1. Required arguments
+<br>
+(Incase we do not pass arguments with key-value syntax.It is important we pass the arguments in correct positional order and the number of arguments should match total number of arguments)
+<br>
+2. Keyword arguments
+<br>
+(We provide argument with key=value this way the interpreter recognizes the arguments by the parameters name and the order in which the argument is passed does not matter)
+<br>
+3. Default arguments
+<br>
+(We provide the function with a default value in this way the function assumes those default values if arguments are not given when function is called)
+<br>
+4. Variable length arguments
+<br>
+(Somtimes we need more arguments than those defined in the actual function.We can do it in 2 ways)
+<br>
+-->Arbitary Arguments
+<br>
+(We pass a * in the function with parameter name while defining it.The function accesses them and processes them in the form of a tuple)
+<br>
+-->Arbitary keyword arguments
+<br>
+(We pass a ** in the function with parameter name while defining it.The function accesses them and processes
+them in the form of a dictionary)
 
-->Note:-
-<br>
-1-String slicing works as string[starting_index,ending_index]
-<br>
-2-If your index goes out of bound in index slicing it does not give  error it is different from indexing 
-<br>
-3-Begin should be smaller then end if you want to print something within range
-<br>
-4- Fruit[1:4] in this case the characters printed will start from 1 but will not include 4 in them
-<br>
