5e9788f3875e8860b55cc13b2dad212e498734d2
HamzaFarooq3333 <hamzafarooqsea@gmail.com>
2025-01-11 16:43:18 +0500

    Added info on Doc-string and Pip 8

--- Code.py
+++ Code.py
@@ -1,44 +1,11 @@
-#f-string
+#Doc-string
 
-letter ="Hello ,My name is {} and I live in {}"
-name="Hamza"
-country="Pakistan"
-print(letter.format(name,country))
+def square(a):    #The docstring should be defined before function body otherwise we will get nothing 
+    '''Takes in a number and returns the square of that number '''
+    print(a*a)
+  
 
-#if it is given in this order
-print(letter.format(country,name))
+square(5)
+print(square.__doc__) #Displays the docstring we wrote in a function in  python 
 
-#To fix above mistake we used to do formatting like this 
-letter ="Hello ,My name is {1} and I live in {0}"
-name="Hamza"
-country="Pakistan"
-print(letter.format(country,name))
-
-#The above method is correct but it is not convinient to use.Therefore we use f-string method
-print(f"Hello my name is {name} and I live in {country}")
-
-#f-string allows us to populate the value of the varaiables directly in the string 
-
-
-#Rounding off value using old method 
-
-txt="The price of apple is {value:.2f} dollars"  #2f with the variable rounds of the value upto 2 decimal places
-print(txt.format(value=2.42743847))       #Similar is done with 3f and 4f onward
-
-
-#Rounding off value using f-string
-price=2.34357
-txt=f"The price of apple is {price:.2f} dollars"
-print(txt)
-
-
-#Single statement f-string
-print(f"{2* 30}")
-
-
-#f-string to print a strig without populating values in the string
-print(f"My name is {{name}} and I live in {{country}}") #We use double curly brackets to avoid population of values 
-
-#This retains the f-string as an f-string
-
-
+import this  #Pep 8 (Python Enhancement Proposal)
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-F-string is a method that helps us conviniently place variables in a string.It is used in string formating 
+Docstring and pep_8 important topics asked in interview.
 <br>
-f-string is introduced in python 3.6 and onward.Its type is also a string
+Doc-string is a string literal that appears right after the defination of a function,method,class, or module.
 <br>
-We use double curly brackets in order to avoid the f-string from populating the values of the variable 
+Doc-strings are not ignored like comments they are placed in strings above the function body.They contain information about the function like what is its input and what output it will give.
+<br>
+Changing comment does not change the output of the program but changing doc-string change the output of the program 
+<br>
+Pep 8 (Python Enhancement Proposal) .It contains a document that tells us that how we can write a good and maintainable python code 
+<br>
+Write import this in command prompt after writing python in case of windows 
+<br>
+It gives a poem that helps the programmer realize the mistakes and improve their big projects in python 
