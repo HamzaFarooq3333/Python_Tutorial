3f3304f61c9c5c0e58d4dfcfac1a903f1a505da0
HamzaFarooq3333 <hamzafarooqsea@gmail.com>
2025-01-20 12:18:39 +0500

    Added info on Exception Handling

--- Code.py
+++ Code.py
@@ -1,179 +1,38 @@
-#Dictionary 
 
-info={
-  "Name":"Hamza Farooq",
-  "Age":20,
-  "City":"Lahore",
-  "Country":"Pakistan"
 
-}
+#Error handling
 
-#Accessing single value
-#We can acces the name which is Hamza Farooq using the key name  
-print(info["Name"])   #This will throw error in case the key is not present
-print(info.get("Name"))  #This will print none in case the key is not present
+a=input('enter a number : ')
+print(f"Multilpication table of {a} is : ")
+
+
+#I will get the exception and the other lines of code keep on executing 
+try:
+   for i in range(1,11):
+     print(f"{int(a)} x {i} = {int(a)*i}")
+except Exception as exception:
+   print('The program could not be run becasue of following error')
+   print(str(exception))
+
+
+print('Some line of code')
+print('End of code')
+
+
+
+#Handling a specific type of error a number  error and index error we  can handle different types of errors 
+# in a single python code 
 print('\n')
-
-
-
-#Acessing multiple values
-#We can access multiple values using the .items() method which returns a list of tuples
-for key, value in info.items():
-    print(f" The value corresponding to the key  {key}  is : {value}")  #This will print all the key value pairs
-
-print('\n')
-
-#Priting the key value pairs
-print(info.items())
-
-print('\n')
-for key,value in info.items():
-   print(f" The value corresponding to the key  {key}  is : {value}")  #This will print all the key value pairs
-
-
-print('\n')
-
-
-#Creating empty dictionary 
-#We can create an empty dictionary using the {} syntax
-empty_dict={}  #This is an empty dictionary
-print("Printing empty dictionary : ",empty_dict)
-print('\n')
-
-
-
-#Dictionary Methods
-#1-Update (Used to update the items in the dictionary)
-ep1={122:45,123:89,145:67,324:76}
-ep2={666:32,324:36,124:46}
-print("ep1 before update : ",ep1)
-ep1.update(ep2)
-print("ep1 after update : ",ep1)
-print('\n')
-
-
-
-#2-Clear (Removes all the items from the dictionary)
-ep1.clear()
-print("ep1 after clearing : ",ep1)
-ep1={122:45,123:89,145:67,324:76}
-print('\n')
-
-
-
-#3-Pop (Removes the required key value pair from the dictionary )
-print('\n')
-print('ep2 before using pop function ',ep2)
-ep2.pop(324)
-print('ep2 after using pop to remove key value pair with key 324',ep2)
-print('\n')
-
-
-
-
-
-#4-PopItem (Removes the last key value pair from the dictionary)
-print('\n')
-print('ep1 before using popitem ', ep1)
-ep1.popitem()
-print('ep1 after using popitem to delete the last key value pair',ep1)
-print('\n')
-
-
-
-
-#5-del (Deletes the dictionary )
-print('\n')
-print('Before deleting key in ep1',ep1)
-del ep1[123]  #Write the key as an integer you will get error if you write it as a string 
-print('Deleting key value pair having key 123 using del keyword',ep1)
-del ep1  #Del keyword will delete the entire doctionary if the key value is  not given 
-print("deleted ep1 using del keyword")
-print('\n')
-
-
-
-
-#6-Copy (Copies the keys and values of a dctionary and places it in another dictionary)
-print('\n')
-a={2:32,3:45,4:54}
-b={7:32}
-b=a.copy()
-print("a : ",a)
-print("b : ",b)
-print('\n')
-
-
-
-
-#7-Set Default (Adds  a key in the dictionary without a value)
-print('\n')
-a.setdefault(9)
-print(a)
-print('\n')
-
-
-
-
-#8-FromKey    (Returns a new dictionary with the specified keys and values.
-# You will get a new dictionary having all the keys in the input dictionary having the same input value)
-print('Printing b using fromkeys : ',a.fromkeys(b,53))
-print('\n')
-
-
-
-
-#9-Values (Returns all the values of the dictionary )
-print("Printing values of a : ",a.values())
-print('\n')
-
-
-
-
-
-
-#10-Keys  (Returns all the keys of the dictionary)
-print("Printing keys of a : ",a.keys())
-print('\n')
-
-
-
-#11-Items (Returns all the items(key-value pair) in the dictionary)
-print("Printing items in a",a.items())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('Handling value error ')
+
+try:
+   num=int(input("Enter a number = "))
+   a=[4,6]
+   print(a[num])
+
+except ValueError:
+   print('Number entered is not an integer')
+except IndexError:
+   print('Index out of range')
+except IsADirectoryError:
+   print('The path specified is a directory')
--- README.md
+++ README.md
@@ -1,36 +1,9 @@
-Dictionaries are ordered collection of data items.They store multiple values in a single variable.Dictionary items are key values-pair they are seperated by comma and enclosed in curly brackets.
+Exception handling is a process of handling the runtime errors or exceptions that occur during the execution of a program
 <br>
-The values in a dictionary can be excessed using keys which hold the specific value.
+Exception handling is responding to unexpected and unwanted events when our computer program runs.Exception handling deals with these program or events to avoid the program from crashing and without this process the exceptions would disrupt the normal operation of a program
 <br>
-The use is to create a mapping somewhat similar to  hashmaps that  are used in c++.
+We use try ,except  for error handling  
 <br>
-Methods used in dictionaries are follwoing:
+This keeps our program to continue the other lines of code despite the exception occuring  in the above code 
 <br>
-1-Pop  (Removes the key value pair whose key value is inserted)
-<br>
-2-Popitem (Removes the last inserted key value pair)
-<br>
-3-Clear (Removes all key value pairs)
-<br>
-4-Keys (Returns a list of all keys)
-<br>
-5-Values (Returns a list of all values)
-<br>
-6-Items (Returns a list of all key value pairs)
-<br>
-7-Update (Updates the dictionary with the items from another dictionary or from an iterable of key-value)
-<br>
-8-Get (Returns the value for the given key if it exists in the dictionary)
-<br>
-9-Setdefault (Adds a key in the dictionary without a value paired to it )
-<br>
-10-Fromkeys (Returns a new dictionary with the specified keys and values.You will get a new dictionary having all the keys in the input dictionary having the same input value)
-<br>
-11-Get (Returns the value for the given key if it exists in the dictionary)
-<br>
-12-Copy   (Copies the keys and values of a dctionary and places it in another dictionary)
-<br>
-13-Items (Returns a list of all key value pairs)
-
-
-
+We can also handle a specific type of error like value error ,server data error
