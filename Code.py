e96408ef4c8f8bb9bcf4ce77501cce308541457c
HamzaFarooq3333 <hamzafarooqsea@gmail.com>
2025-01-22 22:48:56 +0500

    Added info on Emurate_Function

--- Code.py
+++ Code.py
@@ -1,27 +1,57 @@
 
-
-a=300
-b=3005
-
-#Short hand if-else 
-print('A') if a>b else  print('=') if a==b else print("B")
-
-print('\n')
-
-#We can make a new variable by using conditions on the previous result output 
-#The syntax is      Result = value_if_true  if condition else value_if_false
-
-c=4 if a>b else 7
-print('Printing value of C determined by value of a and b : ',c)
-
-
-
+#Using list 
+marks=[12,34,65,87,9,33,4,27]
+print('index value')
+index=0
+for mark in marks:
+    print(index,'   ',mark)
+    if(index==3):
+        print("This is the 4th mark")
+    index=index+1
 
 print('\n')
-def fun1(a):
-    return a*a
+print("-> Using enumerate for list ")
 
-def fun2(a):
-    return a+a
+#Using emurate function 
+print('index value')
+for index,mark in enumerate(marks,start=3):
+    print(index,'   ',mark)
+    if(index==3):
+        print("This is the 4th mark")
 
-print("A") if a>b else print(fun1(a)) if a==b else print(fun2(a)) 
+
+
+#Using string 
+print('\n')
+string="Hamza_Farooq"
+print("-> Using enumerate for string ")
+#Using emurate function
+print('index value')
+for index,character in enumerate(string):
+    print(index,"   ",character)
+    if(index==3):
+        print("This is the 4th character")
+
+
+
+
+
+#Using Tuples
+print('\n')
+#Using emurate function
+print("-> Using enumerate for Tuples ")
+marks=(12,34,65,87,9,33,4,27)
+print('index value')
+for index,mark in enumerate(marks):
+    print(index,"   ",mark)
+    if(index==3):
+        print("This is the 4th mark")
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-Short hand if-else is a convinient way to write simple if-else statements especially when you want to assign value to a variable based on a condition.It is not good for complex logic statments use simple if-else in that case.
+Enumerate function is a bulit in function that allows us to loop over a sequence (such as list ,tuple, or string) and get the index and value of each element in the sequence at the same time 
 <br>
-Short hand if else statement are basically if-else conditions executed in a short method in our code 
-<br>
-This helps us in writing simple and clear code
-<br>
-It can also be used to assign different values to a variable based on the output from the previuos result.
-<br>
-        Result = value_if_true  if condition else value_if_false
-<br>
-Elif cannot be used in the same way in a short method like short hand if-else.
+The indexing is handled by enumerate function by itself and we do not  have  to define an extra index variable for it.The index mostly starts from 0
